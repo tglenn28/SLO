fb649883285a2388489f48767479b0cf2e73ea80
Austin Jeffries <20796052+ajeffries0492@users.noreply.github.com>
2020-05-01 00:36:47 -0400

    Delete files

--- iSAM_ICP_on_LIDAR/Generate_Transformations/For_00_no_Cov/loadTransformations00.m
+++
@@ -1,5 +0,0 @@
-function [T_SICP, T_GICP] = loadTransformations00(file)
-    transforms = load(file);
-    T_SICP = transforms(1:4,:);
-    T_GICP = transforms(5:8,:);
-end
--- iSAM_ICP_on_LIDAR/Generate_Transformations/For_07_03_with_Cov/loadTransformations03.m
+++
@@ -1,8 +0,0 @@
-function [T_SICP, T_GICP, cov, MSE] = loadTransformations03(file)
-    transforms =  importdata(file, " ", 1);
-    T_SICP = reshape(transforms.data(end-31:end-16),4,4)';
-    T_GICP = reshape(transforms.data(end-15:end),4,4)';
-    cov = reshape(transforms.data(1:36),6,6);
-    
-    MSE = str2double(erase(transforms.textdata, "MSE: "));
-end
